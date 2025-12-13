a5f119885f5e6afcdca45a6263208d5c0b259e23
shirai <haranokuroi@gmail.com>
2025-12-14 03:44:21 +0900

    Remove InvalidExpressionException from exception handling in TestOneInput function

--- fuzz/fuzz_string_calculator.py
+++ fuzz/fuzz_string_calculator.py
@@ -31,7 +31,7 @@ def TestOneInput(data):
       # StringCalculatorのcalculateメソッドを実行
       calc.calculate(expression)
             
-   except (ValueError, InvalidInputException, InvalidExpressionException):
+   except (ValueError, InvalidInputException):
    #    # 0除算、入力範囲外、不正な式形式のエラーは仕様通りなので無視
       pass
    except Exception as e:
